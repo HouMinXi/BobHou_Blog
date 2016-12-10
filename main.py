34b6d8b47a18c23437b85d5be5cca04a37e449bf
BobHou <houminxi@gmail.com>
2016-12-10 18:02:37 +0800

    Build SQL model

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from config import DevConfig
 from flask.ext.sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
@@ -18,10 +19,49 @@ class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(255))
     password = db.Column(db.String(255))
+    posts = db.relationship(
+        'Post',
+        backref='user',
+        lazy='dynamic',
+    )
 
+    def __init__(self, username):
+        self.username = username
 
-def __init__(self, username):
-    self.username = username
+    def __repr__(self):
+        return "<User '{}'>".format(self.username)
 
-def __repr__(self):
-    return "<User '{}'>".format(self.username)
+class Post(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    publish_date = db.Column(db.DateTime())
+    comments = db.relationship(
+        'Comment',
+        backref='post',
+        lazy='dynamic'
+    )
+
+    user_id = db.Column(db.Integer(), db.ForeignKey('user_table_name.id'))
+
+    def __init__(self, title):
+        
+        self.title = title
+        '''
+        self.text = text
+        if publish_date is None:
+            publish_date = datetime.utcnow()
+        '''
+
+    def __repr__(self):
+        return "<Post '{}'>".format(self.title)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer(),primary_key=True)
+    name = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    date = db.Column(db.DateTime())
+    post_id = db.Column(db.Integer(), db.ForeignKey('post.id'))
+
+    def __repr__(self):
+        return "<Comment '{}'>".format(self.text[:15])
